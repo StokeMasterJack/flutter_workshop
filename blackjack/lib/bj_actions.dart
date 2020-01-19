ed2cf57b69e6f9c4b5f05bfa96f475c647bfcd23
David Ford <dford@smart-soft.com>
2020-01-19 02:09:39 -0800

    clean up actions

--- blackjack/lib/bj_actions.dart
+++ blackjack/lib/bj_actions.dart
@@ -5,46 +5,6 @@ enum UI { ui1, ui2 }
 enum Page { home, game1, game2 }
 enum BjEvent { deal, hit, stay }
 
-@sealed
-abstract class Action {
-  Type get type => runtimeType;
-
-  const Action();
-
-  bool get isNav => false;
-
-  bool get isBj => false;
-}
-
-class NavAction extends Action {
-  final Page page;
-  final String title;
-
-  @override
-  bool get isNav => true;
-
-  const NavAction(this.page, this.title);
-
-  static NavAction home = NavAction(Page.home, "Home");
-  static NavAction game1 = NavAction(Page.game1, "Play Game ${UI.ui1.title}");
-  static NavAction game2 = NavAction(Page.game2, "Play Game ${UI.ui2.title}");
-}
-
-class BjAction extends Action {
-  final BjEvent event;
-
-  const BjAction(this.event);
-
-  @override
-  bool get isBj => true;
-
-  static BjAction deal = BjAction(BjEvent.deal);
-  static BjAction hit = BjAction(BjEvent.hit);
-  static BjAction stay = BjAction(BjEvent.stay);
-}
-
-typedef Dispatch = void Function(Action action);
-
 extension UIExt on UI {
   String get name => this.toString().split('.').last;
 
@@ -59,6 +19,52 @@ extension UIExt on UI {
     }
   }
 }
+
+extension PageExt on Page {
+  String get name => this.toString().split('.').last;
+
+  String get title {
+    switch (this) {
+      case Page.home:
+        return "Home";
+      case Page.game1:
+        return "Play Game ${UI.ui1.title}";
+      case Page.game2:
+        return "Play Game ${UI.ui2.title}";
+      default:
+        throw StateError("");
+    }
+  }
+}
+
+@sealed
+abstract class Action {
+  const Action();
+}
+
+class NavAction extends Action {
+  final Page page;
+
+  const NavAction(this.page);
+
+  String get title => page.title;
+
+  static NavAction home = NavAction(Page.home);
+  static NavAction game1 = NavAction(Page.game1);
+  static NavAction game2 = NavAction(Page.game2);
+}
+
+class BjAction extends Action {
+  final BjEvent event;
+
+  const BjAction(this.event);
+
+  static BjAction deal = BjAction(BjEvent.deal);
+  static BjAction hit = BjAction(BjEvent.hit);
+  static BjAction stay = BjAction(BjEvent.stay);
+}
+
+typedef Dispatch = void Function(Action action);
 
 class UICtx extends InheritedWidget {
   final UI ui;
